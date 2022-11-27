a6c94d95d9cffe11c51e9f75015266bb129fcf59
731676158 <731676158@qq.com>
2022-11-27 21:03:22 +0800

    m

--- src/lidar_localization/cmake/PCL.cmake
+++ src/lidar_localization/cmake/PCL.cmake
@@ -1,5 +1,8 @@
 find_package(PCL 1.7 REQUIRED)
 list(REMOVE_ITEM PCL_LIBRARIES "vtkproj4")
 
+add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/pcl)
+include_directories(${PROJECT_SOURCE_DIR}/third_party/pcl/include/)
+
 include_directories(${PCL_INCLUDE_DIRS})
 list(APPEND ALL_TARGET_LIBRARIES ${PCL_LIBRARIES})
